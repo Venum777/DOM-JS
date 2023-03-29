d7672d53130709c3cee945a908a84dac4c39d4bb
unknown <venums46@gmail.com>
2023-03-29 14:04:46 +0600

    Dom JavaScript

---
+++ app.js
@@ -0,0 +1,34 @@
+//1 Задание
+// const print=document.querySelectorAll('.mini_list>li');
+// console.log(print[1]);
+
+//2 Задание
+// const change=document.querySelectorAll('.second_block>ul>li');
+// change[2].style.cssText = `
+// font-weight: bold;
+// font-style: italic;
+// text-decoration: underline;
+// `;
+
+//3 Задание
+// const change=document.querySelectorAll('.second_block>ul>li');
+// const newElement=document.createElement('ul');
+// newElement.innerHTML = `
+// <li>Кирилл Иванович</li>
+// <li>Александра Иванович</li>
+// `;
+// change[1].append(newElement)
+
+//4 Задание
+// const change=document.querySelectorAll('.list>li');
+// change[1].innerHTML=`
+// <li>Корчевский София Ивановна</li>
+// `
+
+//5 Задание
+// const change=document.querySelectorAll('.list>li');
+// const change2=document.querySelectorAll('.mini_list>li');
+// console.log(change)
+// change2[3].innerHTML += ' Дмитриевна'
+// change[4].innerHTML += ' Дмитриевич';
+// change[5].innerHTML += ' Александрович';
